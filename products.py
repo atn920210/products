182598c6ab158c55bf279511ebfa9135dc26fe0a
Austin PC Chen <atn920210@gmail.com>
2021-04-26 19:45:57 +0800

    add check file exist function

--- products.py
+++ products.py
@@ -1,13 +1,19 @@
-# 讀取檔案
+import os #operating system 作業系統
 products = []
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line: 
-			continue # 迴圈裡的限定功能, 如果有'商品,價格'跳到下一回, 但仍在迴圈內
-		name, price = line.strip().split(',') #在 line 裡面執行去掉\n 的功能, 並在每個 ','做分割
-		# 上面直接在分割後分別儲存在 name 和 price
-		products.append([name, price])
-print(products)
+#檢查檔案是否存在
+if os.path.isfile('products.csv'):
+	print('yeah! 找到檔案了!')
+# 讀取檔案
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品,價格' in line: 
+				continue # 迴圈裡的限定功能, 如果有'商品,價格'跳到下一回, 但仍在迴圈內
+			name, price = line.strip().split(',') #在 line 裡面執行去掉\n 的功能, 並在每個 ','做分割
+			# 上面直接在分割後分別儲存在 name 和 price
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案...')
 
 #讓使用者輸入
 while True:
